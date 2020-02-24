31f46ec68ae43a03cd72003b4d05f2386e1b4c84
FKeir <fmkeir@gmail.com>
2020-02-24 00:13:22 +0000

    Refactor tables to separate file for clarity

--- app.js
+++ app.js
@@ -1,140 +1,11 @@
-const races = {
-  bugbear: {
-    attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
-    traits: ["martial"]
-  },
-  dragonborn: {attack: 2, power: 2, defense: 1, toughness: 1, morale: 1,
-    traits: ["courageous"]
-  },
-  Ddarf: {attack: 3, power: 1, defense: 1, toughness: 1, morale: 2,
-    traits: ["stalwart"]
-  },
-  elf: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
-    traits: ["eternal"]
-  },
-  "winged elf": {attack: 1, power: 1, defense: 0, toughness: 0, morale: 1,
-    traits: ["eternal"]
-  },
-  ghoul: {attack: -1, power: 0, defense: 2, toughness: 2, morale: 0,
-    traits: ["undead", "horrify", "ravenous"]
-  },
-  gnoll: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
-    traits: ["frenzy"]
-  },
-  gnome: {attack: 1, power: -1, defense: 1, toughness: -1, morale: 1,
-    traits: []
-  },
-  goblin: {attack: -1, power: -1, defense: 1, toughness: -1, morale: 0,
-    traits: []
-  },
-  hobgoblin: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
-    traits: ["bred for war", "martial advantage"]
-  },
-  human: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
-    traits: ["courageous"]
-  },
-  kobold: {attack: -1, power: -1, defense: 1, toughness: -1, morale: -1,
-    traits: []
-  },
-  lizardfolk: {attack: 2, power: 1, defense: -1, toughness: 1, morale: 1,
-    traits: ["amphibious"]
-  },
-  ogre: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 1,
-    traits: ["brutal"]
-  },
-  orc: {attack: 2, power: 1, defense: 1, toughness: 1, morale: 2,
-    traits: ["savage"]
-  },
-  skeleton: {attack: -2, power: -1, defense: 1, toughness: 1, morale: 1,
-    traits: ["undead", "mindless"]
-  },
-  treant: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 0,
-    traits: ["siege engine", "twisting roots", "rock hurler"]
-  },
-  troll: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 0,
-    traits: ["regenerate"]
-  },
-  zombie: {attack: -2, power: 0, defense: 2, toughness: 2, morale: 2,
-    traits: ["undead", "mindless"]
-  }
-};
-
-const traits = {
-  amphibious: {cost: 50},
-  "bred for war": {cost: 100},
-  brutal: {cost: 200},
-  courageous: {cost: 50},
-  eternal: {cost: 50},
-  feast: {cost: 50},
-  horrify: {cost: 200},
-  martial: {cost: 100},
-  mindless: {cost: 100},
-  regenerate: {cost: 200},
-  ravenous: {cost: 50},
-  "rock hurler": {cost: 250},
-  savage: {cost: 50},
-  stalwart: {cost: 50},
-  "twisting roots": {cost: 200},
-  undead: {cost: 50},
-}
-
-const experiences = {
-  green: {attack: 0, power: 0, defense: 0, toughness: 0, morale: 0},
-  regular: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 1},
-  seasoned: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 2},
-  veteran: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 3},
-  elite: {attack: 2, power: 0, defense: 0, toughness: 2, morale: 4},
-  "super elite": {attack: 2, power: 0, defense: 0, toughness: 2, morale: 5}
-};
-
-const equipments = {
-  light: {attack: 0, power: 1, defense: 1, toughness: 0, morale: 0},
-  medium: {attack: 0, power: 2, defense: 2, toughness: 0, morale: 0},
-  heavy: {attack: 0, power: 4, defense: 4, toughness: 0, morale: 0},
-  "super heavy": {attack: 0, power: 6, defense: 6, toughness: 0, morale: 0}
-};
-
-const unitTypes = {
-  airborne: {
-    attack: 0, power: 0, defense: 0, toughness: 0,
-    morale: 3, costMulti: 2
-  },
-  archers: {
-    attack: 0, power: 1, defense: 0, toughness: 0,
-    morale: 1, costMulti: 1.75
-  },
-  cavalry: {
-    attack: 1, power: 1, defense: 0, toughness: 0,
-    morale: 2, costMulti: 1.5
-  },
-  levies: {
-    attack: 0, power: 0, defense: 0, toughness: 0,
-    morale: -1, costMulti: 0.75
-  },
-  infantry: {
-    attack: 0, power: 0, defense: 1, toughness: 1,
-    morale: 0, costMulti: 1
-  },
-  "siege engine": {
-    attack: 1, power: 1, defense: 0, toughness: 1,
-    morale: 0, costMulti: 1.5
-  }
-};
-
-const unitSizes = {
-  d4: {costMulti: 0.66},
-  d6: {costMulti: 1},
-  d8: {costMulti: 1.33},
-  d10: {costMulti: 1.66},
-  d12: {costMulti: 2},
-};
+const tables = require('./tables')
 
 const createUnit = function (race, experience, equipment, unitType, unitSize) {
   let components = [
-    races[race],
-    experiences[experience],
-    equipments[equipment],
-    unitTypes[unitType]
+    tables.races[race],
+    tables.experiences[experience],
+    tables.equipments[equipment],
+    tables.unitTypes[unitType]
   ];
   unit = {
     name: `${unitSize} ${race} ${experience} ${equipment} ${unitType}`,
@@ -143,15 +14,11 @@ const createUnit = function (race, experience, equipment, unitType, unitSize) {
     defense: sumModifier("defense", components),
     toughness: sumModifier("toughness", components),
     morale: sumModifier("morale", components),
-    traits: races[race]["traits"],
+    traits: tables.races[race]["traits"],
     typeMulti:
-    unitTypes[unitType]["costMulti"] * unitSizes[unitSize]["costMulti"] * 10
+    tables.unitTypes[unitType]["costMulti"] * tables.unitSizes[unitSize]["costMulti"] * 10
   }
   return unit;
-};
-
-const sumModifier = function (trait, components) {
-  return components.reduce((sum, component) => sum += component[trait], 0);
 };
 
 const calculateCost = function (unit){
@@ -162,8 +29,12 @@ const calculateCost = function (unit){
    return result;
 };
 
+const sumModifier = function (trait, components) {
+  return components.reduce((sum, component) => sum += component[trait], 0);
+};
+
 const sumTraitValues = function(traitsArray){
-  return traitsArray.reduce((sum, trait) => sum += traits[trait]["cost"], 0)
+  return traitsArray.reduce((sum, trait) => sum += tables.traits[trait]["cost"], 0)
 }
 
 unit = createUnit("dragonborn", "seasoned", "light", "infantry", "d4")
---
+++ tables.js
@@ -0,0 +1,127 @@
+module.exports = {
+  races: {
+    bugbear: {
+      attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
+      traits: ["martial"]
+    },
+    dragonborn: {attack: 2, power: 2, defense: 1, toughness: 1, morale: 1,
+      traits: ["courageous"]
+    },
+    Ddarf: {attack: 3, power: 1, defense: 1, toughness: 1, morale: 2,
+      traits: ["stalwart"]
+    },
+    elf: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
+      traits: ["eternal"]
+    },
+    "winged elf": {attack: 1, power: 1, defense: 0, toughness: 0, morale: 1,
+      traits: ["eternal"]
+    },
+    ghoul: {attack: -1, power: 0, defense: 2, toughness: 2, morale: 0,
+      traits: ["undead", "horrify", "ravenous"]
+    },
+    gnoll: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
+      traits: ["frenzy"]
+    },
+    gnome: {attack: 1, power: -1, defense: 1, toughness: -1, morale: 1,
+      traits: []
+    },
+    goblin: {attack: -1, power: -1, defense: 1, toughness: -1, morale: 0,
+      traits: []
+    },
+    hobgoblin: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
+      traits: ["bred for war", "martial advantage"]
+    },
+    human: {attack: 2, power: 0, defense: 0, toughness: 0, morale: 1,
+      traits: ["courageous"]
+    },
+    kobold: {attack: -1, power: -1, defense: 1, toughness: -1, morale: -1,
+      traits: []
+    },
+    lizardfolk: {attack: 2, power: 1, defense: -1, toughness: 1, morale: 1,
+      traits: ["amphibious"]
+    },
+    ogre: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 1,
+      traits: ["brutal"]
+    },
+    orc: {attack: 2, power: 1, defense: 1, toughness: 1, morale: 2,
+      traits: ["savage"]
+    },
+    skeleton: {attack: -2, power: -1, defense: 1, toughness: 1, morale: 1,
+      traits: ["undead", "mindless"]
+    },
+    treant: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 0,
+      traits: ["siege engine", "twisting roots", "rock hurler"]
+    },
+    troll: {attack: 0, power: 2, defense: 0, toughness: 2, morale: 0,
+      traits: ["regenerate"]
+    },
+    zombie: {attack: -2, power: 0, defense: 2, toughness: 2, morale: 2,
+      traits: ["undead", "mindless"]
+    }
+  },
+  traits: {
+    amphibious: {cost: 50},
+    "bred for war": {cost: 100},
+    brutal: {cost: 200},
+    courageous: {cost: 50},
+    eternal: {cost: 50},
+    feast: {cost: 50},
+    horrify: {cost: 200},
+    martial: {cost: 100},
+    mindless: {cost: 100},
+    regenerate: {cost: 200},
+    ravenous: {cost: 50},
+    "rock hurler": {cost: 250},
+    savage: {cost: 50},
+    stalwart: {cost: 50},
+    "twisting roots": {cost: 200},
+    undead: {cost: 50},
+  },
+  experiences: {
+    green: {attack: 0, power: 0, defense: 0, toughness: 0, morale: 0},
+    regular: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 1},
+    seasoned: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 2},
+    veteran: {attack: 1, power: 0, defense: 0, toughness: 1, morale: 3},
+    elite: {attack: 2, power: 0, defense: 0, toughness: 2, morale: 4},
+    "super elite": {attack: 2, power: 0, defense: 0, toughness: 2, morale: 5}
+  },
+  equipments: {
+    light: {attack: 0, power: 1, defense: 1, toughness: 0, morale: 0},
+    medium: {attack: 0, power: 2, defense: 2, toughness: 0, morale: 0},
+    heavy: {attack: 0, power: 4, defense: 4, toughness: 0, morale: 0},
+    "super heavy": {attack: 0, power: 6, defense: 6, toughness: 0, morale: 0}
+  },
+  unitTypes: {
+    airborne: {
+      attack: 0, power: 0, defense: 0, toughness: 0,
+      morale: 3, costMulti: 2
+    },
+    archers: {
+      attack: 0, power: 1, defense: 0, toughness: 0,
+      morale: 1, costMulti: 1.75
+    },
+    cavalry: {
+      attack: 1, power: 1, defense: 0, toughness: 0,
+      morale: 2, costMulti: 1.5
+    },
+    levies: {
+      attack: 0, power: 0, defense: 0, toughness: 0,
+      morale: -1, costMulti: 0.75
+    },
+    infantry: {
+      attack: 0, power: 0, defense: 1, toughness: 1,
+      morale: 0, costMulti: 1
+    },
+    "siege engine": {
+      attack: 1, power: 1, defense: 0, toughness: 1,
+      morale: 0, costMulti: 1.5
+    }
+  },
+  unitSizes: {
+    d4: {costMulti: 0.66},
+    d6: {costMulti: 1},
+    d8: {costMulti: 1.33},
+    d10: {costMulti: 1.66},
+    d12: {costMulti: 2},
+  }
+};
